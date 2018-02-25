076abaabd7f6826384c0e6cb044009bd71c98f81
collinhw <35476962+collinhw@users.noreply.github.com>
2018-02-24 17:26:44 -1000

    Task 4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
 function Square(props) {
   return (
       <button className="square" onClick={props.onClick}>
@@ -38,7 +34,6 @@ class Board extends React.Component {
             onClick={() => this.handleClick(i)}
         />
     );
-
   }
 
   render() {
@@ -87,26 +82,6 @@ class Game extends React.Component {
         </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 // ========================================
